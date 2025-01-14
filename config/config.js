84258783a0668df25bf5e204e2f97521b4f0fa03
agust1n <acomba@coopmorteros.coop>
2025-01-14 13:39:25 -0300

    feat: configuracion con variables de entornos

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 require('dotenv').config() // Para cargar las variables de entorno desde un archivo .env
 
 module.exports = {
-	masagua: {
-		username: 'desarrollo',
-		password: process.env.DB_PASS,
-		database: 'masagua_desarrollo',
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT || 3306,
-		dialect: 'mysql',
-	},
+    masagua: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: 'mysql',
+    },
 }
