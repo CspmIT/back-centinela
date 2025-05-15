6e63f3350617e0a8cbd3489b37158685da16135b
agust1n <acomba@coopmorteros.coop>
2025-05-15 07:48:29 -0300

    add migration file for pie charts

--- controllers/Pie.controller.js
+++ controllers/Pie.controller.js
@@ -10,6 +10,10 @@ const createPieChart = async (req, res) => {
         }
         const validChart = DoughnutChartschema.safeParse(baseChart)
 
+        if (!validChart.success) {
+            return res.status(400).json(validChart.error.errors)
+        }
+
         return res.status(201).json({ validChart })
     } catch (error) {
         res.status(400).json({ message: error.message })
