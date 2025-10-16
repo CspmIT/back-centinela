bd3639de691c1ae5844e59f5eb178aa3a10c6ef0
Agustin Aressi <agus_aressi@hotmail.com>
2025-10-16 10:34:12 -0300

    feat: alarmas combinadas

--- models/Alarms.js
+++ models/Alarms.js
@@ -1,38 +1,75 @@
 'use strict'
 const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
     class Alarms extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
         static associate(models) {
+            // Relación con la variable principal
             this.belongsTo(models.InfluxVar, { foreignKey: 'id_influxvars', as: 'variable' })
+            // Relación opcional con variable secundaria
+            this.belongsTo(models.InfluxVar, { foreignKey: 'secondaryVariableId', as: 'secondaryVariable' })
         }
     }
+
     Alarms.init(
         {
-            name: DataTypes.STRING,
-            id_influxvars: DataTypes.INTEGER,
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            id_influxvars: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
             condition: {
                 type: DataTypes.ENUM('>', '<', '=', '>=', '<=', 'entre'),
-              },
-            value: DataTypes.FLOAT,
-            value2: DataTypes.FLOAT,
-            status: DataTypes.BOOLEAN,
+                allowNull: false
+            },
+            value: {
+                type: DataTypes.FLOAT,
+                allowNull: false
+            },
+            value2: {
+                type: DataTypes.FLOAT,
+                allowNull: true
+            },
+            status: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: true
+            },
             repeatInterval: {
                 type: DataTypes.INTEGER,
                 allowNull: false,
-                defaultValue: 0,
-              }
+                defaultValue: 0
+            },
+            type: {
+                type: DataTypes.ENUM('single', 'combined'),
+                allowNull: false,
+                defaultValue: 'single'
+            },
+            logicOperator: {
+                type: DataTypes.ENUM('AND', 'OR'),
+                allowNull: true
+            },
+            secondaryVariableId: {
+                type: DataTypes.INTEGER,
+                allowNull: true
+            },
+            secondaryCondition: {
+                type: DataTypes.ENUM('>', '<', '=', '>=', '<='),
+                allowNull: true
+            },
+            secondaryValue: {
+                type: DataTypes.FLOAT,
+                allowNull: true
+            }
         },
-        
         {
             sequelize,
             modelName: 'Alarms',
             timestamps: true
         }
     )
+
     return Alarms
 }
