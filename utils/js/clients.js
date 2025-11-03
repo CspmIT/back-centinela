493156bd5121a364a4f63693fea05b7d73895a27
Agustin Aressi <agus_aressi@hotmail.com>
2025-11-03 10:44:33 -0300

    feat: calculo de variables desde el back

--- utils/js/clients.js
+++ utils/js/clients.js
@@ -1,23 +1,23 @@
 // clients.js
 const listClients = [
-    'capyclo', 
-    'cespal', 
-    'coopmorteros', 
-    'san_genaro',
-    'adeco',
-    'lactear',
-    //'desarrollo'
+    // 'capyclo', 
+    // 'cespal', 
+    // 'coopmorteros', 
+    // 'san_genaro',
+    // 'adeco',
+    // 'lactear',
+    'desarrollo'
   ]
   
   // Mapeo hardcodeado de cada cliente a su influx_name correspondiente
   const influxByClient = {
-    capyclo: 'Sensors_ExternosPublic',
-    cespal: 'Sensors_ExternosPublic',
-    coopmorteros: 'Sensors_Morteros',
-    san_genaro: 'Sensors_ExternosPublic',
-    adeco: 'Sensors_Morteros',
-    lactear: 'Sensors_Morteros',
-    // desarrollo: 'Sensors_ExternosPublic',
+    // capyclo: 'Sensors_ExternosPublic',
+    // cespal: 'Sensors_ExternosPublic',
+    // coopmorteros: 'Sensors_Morteros',
+    // san_genaro: 'Sensors_ExternosPublic',
+    // adeco: 'Sensors_Morteros',
+    // lactear: 'Sensors_Morteros',
+    desarrollo: 'Sensors_ExternosPublic',
   }
   
   module.exports = {
